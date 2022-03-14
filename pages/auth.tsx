a2033f3fd2d592226d14a3186bc7748e81f4b205
frankkiv <lufrankkiv@gmail.com>
2022-03-14 11:13:33 +0800

    update package.json

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,4 @@
 // auth.tsx
-import React from "react";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import {firebaseAuth, firebaseUIConfig} from "../firebase/clientApp";
 
